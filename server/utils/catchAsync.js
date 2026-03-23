f71cc5e1cd94385df799f414c1f76c94daace3d5
Christian Leo Cereno <christiancereno424@gmail.com>
2026-03-23 18:34:45 +0800

    delete all the files for backend

--- server/utils/catchAsync.js
+++
@@ -1,8 +0,0 @@
-const catchAsync = (fn) => {
-  return (req, res, next) => {
-    // Executes the controller function and automatically catches any rejected promises
-    fn(req, res, next).catch(next);
-  };
-};
-
-module.exports = catchAsync;
