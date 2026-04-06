39249aa7a39aac20f103f430cc22b8c5682aa901
Christian Leo Cereno <christiancereno424@gmail.com>
2026-04-06 20:02:55 +0800

    delete the files

--- server/database/migrations/002_customer_invites.sql
+++
@@ -1,2 +0,0 @@
--- Allow expires_at to be NULL for permanent Customer Welcome Links
-ALTER TABLE user_invitations ALTER COLUMN expires_at DROP NOT NULL;
--- server/database/migrations/016_add_healthy_stock_to_inventory.sql
+++
@@ -1,4 +0,0 @@
--- Migration: 016_add_healthy_stock_to_inventory 
--- Description: Add healthy_stock_per_branch to master_inventory
-ALTER TABLE master_inventory
-ADD COLUMN IF NOT EXISTS healthy_stock_per_branch INTEGER DEFAULT 10;
